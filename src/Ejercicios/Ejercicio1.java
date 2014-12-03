1095a7632b525cfb2fe02e44b6e172c843e112d3
cesar_ar <cesar_ar@192.168.1.199>
2014-12-03 09:39:10 -0600

    Primer Ejercicio

---
+++ src/Ejercicios/Ejercicio1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicios;
+
+/**
+ *
+ * @author cesar_ar
+ */
+public class Ejercicio1 {
+    
+    public void m(){
+        System.out.println("Metodo 1");
+    }
+}
