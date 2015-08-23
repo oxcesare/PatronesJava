f78cb4b2757ccbb9624606fd085c6eb7acdf7d62
Oxcesare <Oxcesare@192.168.10.35>
2015-08-22 22:27:48 -0500

    Patrón de Diseño  Facade

---
+++ src/Facade/ClienteImpresora.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Facade;
+
+/**
+ *
+ * @author Oxcesare
+ */
+public class ClienteImpresora {
+    public static void main(String[] args) {
+        
+        
+        Impresora a = new Impresora();
+        a.setHoja("A4");
+        a.setTexto("Teknei");
+        a.setColor(true);
+        a.setTipoDocumento("PDF");
+        
+        
+        Impresora b = new Impresora();
+        a.setHoja("A4");
+        a.setTexto("Teknei 2");
+        a.setColor(true);
+        a.setTipoDocumento("PDF");
+        
+        
+        
+        
+    }
+}
---
+++ src/Facade/FachadaImpresoraNormal.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Facade;
+
+/**
+ *
+ * @author Oxcesare
+ */
+public class FachadaImpresoraNormal {
+
+    Impresora impresora;
+
+    public FachadaImpresoraNormal(String texto) {
+        super();
+        impresora = new Impresora();
+        impresora.setColor(true);
+        impresora.setHoja("A4");
+        impresora.setTipoDocumento("PDF");
+        impresora.setTexto(texto);
+    }
+
+    public void imprimir() {
+
+        impresora.imprimirDocumento();
+    }
+}
