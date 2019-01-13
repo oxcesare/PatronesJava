e9a0328a5d083360d27138222c1188e283deb0f3
cesaralducinruiz <cesaralducinruiz@macbook-pro-de-cesar.local>
2019-01-12 23:42:18 -0600

    Patrones de diseño Java 8

--- src/com/mx/patrones/curso/xml/CreateXmlFileDemo.java
+++ src/com/mx/patrones/curso/xml/CreateXmlFileDemo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CursoXML;
+package com.mx.patrones.curso.xml;
 
 
 import javax.xml.parsers.DocumentBuilderFactory;
